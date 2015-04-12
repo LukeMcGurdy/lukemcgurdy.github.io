e69d9bd57350c82da87e954ae94601e51171c108
Luke McGurdy <mcgurdy_99@yahoo.co.uk>
2015-04-12 02:14:52 +0100

    ajor commit - major changes: file structure altered and published first post

---
+++ assets/js/functions.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+
+	// Slider - home page
+	
+	/*$( '.home-slider' ).on( 'cycle-initialized', function(e, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag ) {
+		animate();
+	});*/
+
+//	$('.home-slider').cycle({
+//		fx: 'scrollHorz',
+//		slides: '.slide',
+//		speed:    1000, 
+//		timeout:  15000,
+//		pager: '#home-pager',
+//		pagerTemplate: '<span></span>'	
+//	});	
+
+	//$('.slide figcaption').addClass('animated fadeInRight');
+
+	$('.slide p:last-of-type').hide();
+    setTimeout(function() {
+	    $(this).fadeIn(1000);
+    },20000);
+	
+
+	/*$( '.home-slider' ).on( 'cycle-pager-activated', function(event, optionHash) {
+		clearTimeout(timer1);
+		clearTimeout(timer2);
+		$('.home-slider .slide figcaption').hide();
+	});
+	
+	$( '.home-slider' ).on( 'cycle-after', function(e, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag ) {
+		animate();
+	});
+	
+	function animate() {
+		$('.home-slider .slide figcaption').removeClass('animated fadeOutLeft').hide();
+		timer1 = setTimeout(function(){
+			$('.home-slider .slide figcaption').addClass('animated fadeInRight').show();
+			timer2 = setTimeout(function(){
+				$('.home-slider .slide figcaption').removeClass('animated fadeInRight').addClass('animated fadeOutLeft');
+        	},14000);
+		},100);
+	}*/
+	
+	
+});
